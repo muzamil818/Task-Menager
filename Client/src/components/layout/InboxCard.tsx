ebd629d9ab7b8c217d1d9bb62c6c4d749baa9273
muzamil al <muzamilali.dev@gmail.com>
2026-07-10 23:22:41 +0500

    setup react tailwind and created navbar

---
+++ Client/src/components/layout/InboxCard.tsx
@@ -0,0 +1,13 @@
+
+const InboxCard = () => {
+    return (
+        <div className="h-[70vh] w-[25vw] bg-[hsl(214,91.3%,34.1%)] rounded ml-4 my-auto">
+            <div className="flex justify-center items-center text-center pt-4">
+                <h1 className="text-white">Inbox</h1>
+
+            </div>
+        </div>
+    )
+}
+
+export default InboxCard
